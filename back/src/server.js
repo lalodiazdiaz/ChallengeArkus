6c983428cc52a8b91043d79c80095a02d81504cc
lalodiazdiaz <eduardodiazdiaz93@gmial.com>
2023-03-14 15:33:41 -0600

    dependencies

--- back/src/server.js
+++ back/src/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-
+const cors = require("cors");
 const app = express();
 
 require("dotenv").config();
@@ -10,7 +10,22 @@ mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => console.log("Connected to MongoDB"))
   .catch((error) => console.error(error));
+
+app.use(cors());
+
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Authorization, Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
+
 app.use(bodyParser.json());
 
 app.use(
