049e935fd15563682418345c73a9befb2cdb996c
lalodiazdiaz <eduardodiazdiaz93@gmail.com>
2023-03-19 20:30:59 -0600

    change

--- front/src/Components/Login/Login.jsx
+++ front/src/Components/Login/Login.jsx
@@ -1,12 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import { useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
 import { useFormik } from "formik";
-import {
-  ArrowRightOnRectangleIcon,
-  EyeIcon,
-  EyeSlashIcon,
-} from "@heroicons/react/24/outline";
+import { ArrowRightOnRectangleIcon } from "@heroicons/react/24/outline";
 import { ROL } from "../../constants";
 import login from "../../Services/LoginService";
 
@@ -26,15 +22,6 @@ const validate = (values) => {
 function Login() {
   const navigation = useNavigate();
 
-  const [passwordType, setPasswordType] = useState("password");
-
-  const togglePassword = () => {
-    if (passwordType === "password") {
-      setPasswordType("text");
-      return;
-    }
-    setPasswordType("password");
-  };
   const formik = useFormik({
     initialValues: {
       email: "",
@@ -120,26 +107,15 @@ function Login() {
                   <input
                     id="password"
                     name="password"
-                    type={passwordType}
+                    type="password"
                     className="inline-block h-10 w-full rounded-l bg-white p-2 leading-none text-black placeholder-indigo-900 shadow placeholder:opacity-30 border-r-0"
                     placeholder="Password"
                     onChange={formik.handleChange}
                     value={formik.values.password}
                   />
-                  {passwordType === "password" ? (
-                    <EyeIcon
-                      className="h-10 w-10  bg-gray-100 border rounded-r-md border-gray-500 text-gray-500 cursor-pointer p-2 hover:bg-gray-300"
-                      onClick={togglePassword}
-                    />
-                  ) : (
-                    <EyeSlashIcon
-                      className="h-10 w-10  bg-gray-100 border rounded-r-md border-gray-500 text-gray-500 cursor-pointer p-2 hover:bg-gray-300"
-                      onClick={togglePassword}
-                    />
-                  )}
                 </div>
-                {formik.errors.email ? (
-                  <div className="mt-2">{formik.errors.email}</div>
+                {formik.errors.password ? (
+                  <div className="mt-2">{formik.errors.password}</div>
                 ) : null}
               </div>
 
--- front/src/Services/LoginService.js
+++ front/src/Services/LoginService.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+
 const API = `${process.env.REACT_APP_API_URL}/login`;
 
 const login = async (email, password) => {
